acd083be799d9f29878cc14c79ce665d0a795955
Shivanshu <shivanshu.coll@gmail.com>
2022-05-28 23:13:55 +0530

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -9,3 +9,20 @@ public class helloWorld {
 
 
 // Language: java
+
+
+//function to find exponent of a number
+public class Exponent {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = input.nextInt();
+        System.out.println("Enter an exponent: ");
+        int exp = input.nextInt();
+        int result = 1;
+        for (int i = 0; i < exp; i++) {
+            result *= num;
+        }
+        System.out.println(num + " to the power of " + exp + " is " + result);
+    }
+}
